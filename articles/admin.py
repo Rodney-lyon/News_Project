f97e4498cab05b3eaa86fdf5383775af32ad93a1
Rodney.M <Y-Rodney@outlook.com>
2018-11-19 21:44:51 +0800

    add comments to Newsapp
    add comments to Newsapp

--- articles/admin.py
+++ articles/admin.py
@@ -2,4 +2,19 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 
-admin.site.register(models.Article)
+
+# class CommentInline(admin.StackedInline):
+#     model = models.Comment
+
+
+class CommentInline(admin.TabularInline):
+    model = models.Comment
+    
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+
+admin.site.register(models.Article, ArticleAdmin)
+admin.site.register(models.Comment)
